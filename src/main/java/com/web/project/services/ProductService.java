638699fa278f6069786a2ecb430896f07a1d0fe2
Eliezer Fonseca <eliezerfonseca2003@gmail.com>
2024-08-27 10:23:19 -0300

    Product

---
+++ src/main/java/com/web/project/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.web.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.web.project.entities.Product;
+import com.web.project.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
