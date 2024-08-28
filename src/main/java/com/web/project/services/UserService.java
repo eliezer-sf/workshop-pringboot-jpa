a2c4e82bf9b65ab194f61cdcd52d5f4a6831e562
Eliezer Fonseca <eliezerfonseca2003@gmail.com>
2024-08-28 15:38:19 -0300

    Exception handling - update

--- src/main/java/com/web/project/services/UserService.java
+++ src/main/java/com/web/project/services/UserService.java
@@ -13,6 +13,8 @@ import com.web.project.repositories.UserRepository;
 import com.web.project.services.exceptions.DatabaseException;
 import com.web.project.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
